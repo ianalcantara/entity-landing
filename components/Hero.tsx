38cf7d91a1b5027d274e50af20a8b4d9afb5faaa
Ian Alcantara <ianalcantara@Ians-MacBook-Pro-2.local>
2024-08-17 00:06:48 +0800

    changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import { MacbookScroll } from './ui/MacbookScroll'
 
 const Hero = () => {
   return (
@@ -18,12 +19,15 @@ const Hero = () => {
                     <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
                         <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Entity Platform</h2>
 
+
                         <TextGenerateEffect 
                             className="text-center text-[40px] md:text-5xl lg:text-6xl"
                             words="Effortlessly manage documents for financial institutions"
                         />
 
                         <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">Hello, this is entity platform test test</p>
+
+                        <MacbookScroll />
                     </div>
                 </div>
         </div>
